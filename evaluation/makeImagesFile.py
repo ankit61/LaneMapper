71442e8905a6c0341b3096fab5f1b44bb180b8a0
Ankit Ramchandani <ankit61@tamu.edu>
2018-06-21 05:12:30 -0500

    Changing evaluation/makeImagesFile.py to produce a sorted list of images

--- evaluation/makeImagesFile.py
+++ evaluation/makeImagesFile.py
@@ -7,7 +7,7 @@ from os import listdir
 from os.path import isfile, join
 import sys
 imageFile = open(sys.argv[2], 'w')
-for f in listdir(sys.argv[1]):
+for f in sorted(listdir(sys.argv[1])):
     if isfile(join(sys.argv[1], f)):
         imageFile.write(f + "\n")
 
