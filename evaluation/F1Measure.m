d75500dc6499443ab17962120257939453db3a20
Ankit Ramchandani <ankit61@tamu.edu>
2018-06-25 18:48:51 -0500

    correcting evaluation scripts to consider all road surfaces

--- evaluation/F1Measure.m
+++ evaluation/F1Measure.m
@@ -1,10 +1,4 @@
-%This script takes as input two directories:
-%    1) where predicted images (segmentation results of Evaluate.cpp)
-%    2) where all ground truth labels are stored in the same format (use convertKITTIImgs.m to convert to desired format) 
-%Then it computes the precision, recall, f1 measure and accuracy using formulae given here (http://www.cvlibs.net/publications/Fritsch2013ITSC.pdf)
-%Finally it prints the mean of f1Measure. By doing trivial changes, you can make it print other quantities of interest too.
-
-clear all
+clear all;
 root = 'C:\Users\Ankit\Documents\TAMU Succeed\Research\Autonomous Driving\segmented\';
 predFiles = dir([root '\ICNet']);
 trueFiles = dir([root '\truth']);
@@ -15,8 +9,6 @@ accuracy = [];
 for i = 3:length(trueFiles)
     pred = imread(char(join([root "ICNet\" predFiles(i).name], '')));
     correct = imread(char(join([root "truth\" trueFiles(i).name], '')));
-    pred = pred(:, :, 1);
-    correct = correct(:, :, 1);
     tp = numel(pred(pred == 255 & pred == correct));
     fp = numel(pred(pred == 255 & pred ~= correct));
     tn = numel(pred(pred == 0 & pred == correct));
