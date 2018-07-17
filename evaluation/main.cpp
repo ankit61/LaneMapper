d2c2923549065ab7b2fc334b1db2e54b3e5e0552
Ankit Ramchandani <ankit61@tamu.edu>
2018-07-17 18:45:10 -0500

    updating main

--- evaluation/main.cpp
+++ evaluation/main.cpp
@@ -1,7 +1,10 @@
 #include "Solver.h"
 
 #include"Segmenter.h"
+
 #include"Refiner.h"
+#include"KPercentExtractor.h"
+
 #include"VeloProjector.h"
 #include"ResultIntersector.h"
 #include"SurfaceDataMaker.h"
@@ -11,6 +14,8 @@
 #include"TLinkage/PlaneTLinkage.h"
 #include"TLinkage/Circle2DTLinkage.h"
 #include"TLinkage/SurfaceTLinkage.h"
+#include"TLinkage/RoadTLinkage.h"
+#include"TLinkage/Line3DTLinkage.h"
 
 #include<pugixml.hpp>
 #include <boost/algorithm/string/predicate.hpp>
@@ -32,8 +37,11 @@ int main(int argc, char* argv[]) {
 
 	if(boost::iequals(solver, "Segmenter"))
 		solverPtr = std::make_unique<Segmenter>(argv[1]);
-	else if(boost::iequals(solver, "Refiner"))
-		solverPtr = std::make_unique<Refiner>(argv[1]);
+	else if(boost::iequals(solver, "Refiner")) {
+		string instance(xml.document_element().child("Solvers").child("Refiner").child("SolverInstance").attribute("instance").as_string());
+		if(boost::iequals(instance, "KPercentExtractor"))
+			solverPtr = std::make_unique<KPercentExtractor>(argv[1]);
+	}
 	else if(boost::iequals(solver, "VeloProjector")) {
 		string instance(xml.document_element().child("Solvers").child("VeloProjector").child("SolverInstance").attribute("instance").as_string());
 		if(boost::iequals(instance, "ResultIntersector"))
@@ -45,14 +53,18 @@ int main(int argc, char* argv[]) {
 	}
 	else if(boost::iequals(solver, "TLinkage")) {
 		string model(xml.document_element().child("Solvers").child("TLinkage").child("SolverInstance").attribute("model").as_string());
-		if(boost::iequals(model, "Line"))
+		if(boost::iequals(model, "Line2D"))
 			solverPtr = std::make_unique<Line2DTLinkage>(argv[1]);
 		else if(boost::iequals(model, "Plane"))
 			solverPtr = std::make_unique<PlaneTLinkage>(argv[1]);
-		else if(boost::iequals(model, "Circle"))
+		else if(boost::iequals(model, "Circle2D"))
 			solverPtr = std::make_unique<Circle2DTLinkage>(argv[1]);
 		else if(boost::iequals(model, "Surface"))
 			solverPtr = std::make_unique<SurfaceTLinkage>(argv[1]);
+		else if(boost::iequals(model, "Road"))
+			solverPtr = std::make_unique<RoadTLinkage>(argv[1]);
+		else if(boost::iequals(model, "Line3D"))
+			solverPtr = std::make_unique<Line3DTLinkage>(argv[1]);
 		else
 			throw runtime_error("No such TLinkage model implemented: " + model);
 	}
