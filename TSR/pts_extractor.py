e5006960c6cecdc8e5881d2827eb9d640aeeb7d3
Ankit Ramchandani <ankit61@tamu.edu>
2018-12-28 20:00:06 +0530

    fixing minor import related bugs ;

--- TSR/pts_extractor.py
+++ TSR/pts_extractor.py
@@ -1,12 +1,15 @@
 import velo_projector
+import sys, os
+sys.path.insert(0, os.path.join(os.path.split(os.path.realpath(__file__))[0], 'utils'))
+import constants
 
 class PtsExtractor(velo_projector.VeloProjector):
     def __init__(self, calib, cam_num = constants.CAM_NUM, min_x = constants.MIN_X):
-        velo_projector.VeloProjector.__init__(calib, cam_num, min_x)
+        velo_projector.VeloProjector.__init__(self, calib, cam_num, min_x)
     
     def extract_pts_inside_bbx(self, bbx, img_pts, lidar_pts):
         '''
-            bbx = (x, y, w, h), (x, y) = top left corner
+            bbx = (x, y, w, h) | (x, y) = top left corner
             img_pts = (x, y, r) or (x, y)
             lidar_pts = (x, y, z, r) or (x, y, z)
 
@@ -15,12 +18,12 @@ class PtsExtractor(velo_projector.VeloProjector):
         #FIXME: can improve speed by using fancy data structures
         pts_in_bbx = []
         for i, pt in enumerate(img_pts):
-            if(self.__inside_box(bbx, pt)):
+            if(self.__is_inside_box(bbx, pt)):
                 pts_in_bbx.append(lidar_pts[i])
         
         return pts_in_bbx
 
-    def __inside_box(self, bbx, pt):
+    def __is_inside_box(self, bbx, pt):
         x = pt[0]
         y = pt[1]
 
