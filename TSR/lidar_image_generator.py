aa22f0b0d4ed8e39de31003f5ed9f93f8a7cbb8c
Ankit Ramchandani <ankit61@tamu.edu>
2019-01-02 01:34:38 +0530

    adding function to refine raw results

--- TSR/lidar_image_generator.py
+++ TSR/lidar_image_generator.py
@@ -1,20 +1,29 @@
 import velo_projector
 import cv2
 from utils import constants
-from utils import LIDARFactor
 import numpy as np
 
 class LIDARImageGenerator(velo_projector.VeloProjector):
     def __init__(self, calib, cam_num = constants.CAM_NUM, min_x = constants.MIN_X):
         velo_projector.VeloProjector.__init__(self, calib, cam_num, min_x)
 
-    def generate(self, velo_points, img_size, intensity_type = LIDARFactor.REFLECTIVITY):
+    def generate_raw(self, velo_points, img_size):
         img_pts_reflectivity = self.project(velo_points)
-        img = np.zeros(img_size, dtype=np.uint8)
-        for (x, y, r, d) in img_pts_reflectivity:
+        lidar_img = np.zeros(img_size[0:2], dtype=np.uint8)
+        for (x, y, r) in img_pts_reflectivity:
             try:
-                img[int(y), int(x)] = int(r * 255) if intensity_type == LIDARFactor.REFLECTIVITY else int(d * 255)
+                lidar_img[int(y), int(x)] = int(r * 255)
             except IndexError:
                 pass
 
-        return img
+        return lidar_img.astype(np.uint8)
+
+    def generate_refined(self, velo_points, img_size, threshold = constants.REFLECTIVITY_THRESH):
+        return self.__refine(self.generate_raw(velo_points, img_size), threshold)
+
+    def __refine(self, lidar_img, threshold = constants.REFLECTIVITY_THRESH):
+        _, refined_img = cv2.threshold(lidar_img, threshold, 255, cv2.THRESH_TOZERO)
+        refined_img = cv2.dilate(refined_img, cv2.getStructuringElement(cv2.MORPH_RECT, (2,2)))
+
+        return refined_img
+    
