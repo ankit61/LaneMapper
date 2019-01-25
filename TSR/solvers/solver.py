92a64a8b5fd65b9ad6792a79579022f68dbf7571
Ankit Ramchandani <ankit61@tamu.edu>
2019-01-25 17:57:48 -0600

    adding progress bar, making visualize function optional to override

--- TSR/solvers/solver.py
+++ TSR/solvers/solver.py
@@ -3,31 +3,33 @@ import cv2
 import numpy as np
 import abc
 from utils import constants
+from tqdm import tqdm
 
 class Solver(abc.ABC):
+
+    class NotVisualizable(NotImplementedError): pass
 
     def __init__(self, base_dir = constants.KITTI_BASE_DIR, date = constants.DATE, drive = constants.DRIVE, cam_num = constants.CAM_NUM):
         self._dataset = pykitti.raw(base_dir, date, drive)
         self.__get_cam = self._dataset.__getattribute__('get_cam' + str(cam_num))
 
-    def __pil_to_cv2(self, pil_img):
+    def pil_to_cv2(self, pil_img):
         img = np.array(pil_img)
         if(img.shape[2] == 3):
             return cv2.cvtColor(img, cv2.COLOR_RGB2BGR)
         elif(img.shape[2] == 1):
             return np.array(img)
 
-    def run(self):
-        for i in range(len(self._dataset)):
-            self.solve(self.__pil_to_cv2(self.__get_cam(i)), self._dataset.get_velo(i), self._dataset.get_base_filename(i))
+    def run(self, start = 0, end = 10000):
+        for i in tqdm(range(start, min(end, len(self._dataset)))):
+            self.solve(self.pil_to_cv2(self.__get_cam(i)), self._dataset.get_velo(i), self._dataset.get_base_filename(i))
 
     def show_result_for(self, idx):
-        self.visualize(self.__pil_to_cv2(self.__get_cam(idx)), self._dataset.get_velo(idx))
+        self.visualize(self.pil_to_cv2(self.__get_cam(idx)), self._dataset.get_velo(idx))
 
     @abc.abstractmethod
     def solve(self, img, velo, base_filename):
         pass
 
-    @abc.abstractmethod
     def visualize(self, img, velo):
-        pass
+        raise NotVisualizable('this can\'t be visualized!')
